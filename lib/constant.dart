2bdc62c5e761cfab86075d0b5309003d5962b810
Abu Anwar MD Abdullah <abir@Abus-MacBook-Pro.local>
2020-03-20 20:51:52 +0600

    constant created

---
+++ lib/constant.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryButtonColor = Color(0xFFE0E2E9);
+const kDefaultButtonColor = LinearGradient(
+  begin: Alignment.topRight,
+  end: Alignment.bottomLeft,
+  colors: [
+    Color(0xFF427DFB),
+    Color(0xFF3E58F7),
+  ],
+);
+const kGradientButtonColorBlack = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    Color(0xFF868F96),
+    Color(0xFF596164),
+  ],
+);
+const kGradientButtonColorBlue = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    Color(0xFF667EEA),
+    Color(0xFF764BA2),
+  ],
+);
+const kGradientButtonColorSkyblue = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    Color(0xFFA1C4FD),
+    Color(0xFFC2E9FB),
+  ],
+);
+const kGradientButtonColorPeach = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    Color(0xFFFF9A9E),
+    Color(0xFFFAD0C4),
+  ],
+);
+const kBackgroundColor = Color(0xFFF3F6FD);
+const kTextColor = Color(0xFF313862);
+const kWhiteColor = Color(0xFFFFFFFF);
